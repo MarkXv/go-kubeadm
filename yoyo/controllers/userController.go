45cae34114e73e38ab90726bd25bbe5b527e3678
markxv <markxvg@gmail.com>
2020-07-03 23:21:14 +0800

    add swagger

--- yoyo/controllers/userController.go
+++ yoyo/controllers/userController.go
@@ -10,19 +10,75 @@ type UserController struct {
 	beego.Controller
 }
 
+type JSON struct {
+	Code string
+	Mag string
+	User string `json:"user_info"`
+
+}
+
+// swagger注解配置
+// @Title Get
+// @Description get user
+// @Success 200 {object} models
+// @Failure 403 error
+// @router / [Get]
 func (c *UserController) Get() {
+	//o := orm.NewOrm()
+
+	//user := models.User{
+	//	//Id:   ,
+	//	Name: "小红",
+	//	Pwd:  "1112",
+	//}
+
+	//_,err := o.Insert(&user)
+	//if err != nil{
+	//	beego.Info("error",err)
+	//	return
+	//}
+	//
+	//res,err := o.QueryTable("user")
+	//c.TplName = "user/userList.html"
+
+	//b,err := json.Marshal(user)
+	//if err != nil {
+	//	fmt.Println("序列化异常")
+	//}
+
+	//查询
+	//o := orm.NewOrm()
+	//
+	//user := models.User{}
+	//user.Id = 1
+	//
+	//err := o.Read(&user)
+	//
+	//if err != nil{
+	//	beego.Info("field")
+	//}
+	//beego.Info(user,"----sucess")
+
+
+	//更新
+
 	o := orm.NewOrm()
 
-	user := models.User{
-		//Id:   ,
-		Name: "小红",
-		Pwd:  "1112",
-	}
+	user := models.User{}
 
-	_,err := o.Insert(&user)
-	if err != nil{
-		beego.Info("error",err)
-		return
+	user.Id = 1
+
+	err := o.Read(&user)
+
+	if err == nil {
+		user.Name = "test"
+		o.Update(&user)
 	}
-	c.TplName = "user/userList.html"
+	o.Read(&user)
+	beego.Info(user)
+
+
+
+	//c.Data["json"] = data
+	c.ServeJSON()
 }
--- yoyo/routers/router.go
+++ yoyo/routers/router.go
@@ -1,4 +1,15 @@
 
+
+// @APIVersion 1.0.1
+// @Title beego Test API for 自定义
+// @Description beego has a very cool tools to autogenerate documents for your API
+// @Contact astaxie@gmail.com
+// @TermsOfServiceUrl http://beego.me/
+// @License Apache 2.0
+// @LicenseUrl http://www.apache.org/licenses/LICENSE-2.0.html
+// 配置路由映射,以上注解必须添加
+
+
 package routers
 
 import (
@@ -7,19 +18,19 @@ import (
 )
 
 func init() {
-	//ns := beego.NewNamespace("/v1",
-	//	beego.NSNamespace("/object",
-	//		beego.NSInclude(
-	//			&controllers.UserController{},
-	//		),
-	//	),
-	//	beego.NSNamespace("/user",
-	//		beego.NSInclude(
-	//			&controllers.UserController{},
-	//		),
-	//	),
-	//)
-	//beego.AddNamespace(ns)
-    beego.Router("/", &controllers.MainController{})
-    beego.Router("/user/add",&controllers.UserController{})
+	ns := beego.NewNamespace("/",
+		beego.NSNamespace("/",
+			beego.NSInclude(
+				&controllers.MainController{},
+			),
+		),
+		beego.NSNamespace("/user/add",
+			beego.NSInclude(
+				&controllers.UserController{},
+			),
+		),
+	)
+	beego.AddNamespace(ns)
+    //beego.Router("/", &controllers.MainController{})
+    //beego.Router("/user/add",&controllers.UserController{})
 }
